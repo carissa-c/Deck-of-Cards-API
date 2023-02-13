e92c99ceccd88a7f70a160b7cee5a8cc83337e0c
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-02-13 17:30:11 -0600

    first upload

---
+++ Models/ShuffleDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Deck_of_Cards_API_Lab.Models
+{
+	public class ShuffleDAL
+	{
+        public static ShuffleDeck GetHand()//adjust here
+        {
+            //Adjust here
+            //Setup
+            string url = "https://deckofcardsapi.com/api/deck/new/draw/?count=5";
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust here
+            //Convert to C#
+            ShuffleDeck result = JsonConvert.DeserializeObject<ShuffleDeck>(JSON);
+            return result;
+        }
+    }
+}
+
---
+++ Models/ShuffleDeck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+
+namespace Deck_of_Cards_API_Lab.Models
+{
+    public partial class ShuffleDeck
+    {
+        public bool Success { get; set; }
+        public string DeckId { get; set; }
+        public Card[] Cards { get; set; }
+        public long Remaining { get; set; }
+    }
+
+    public partial class Card
+    {
+        public string Code { get; set; }
+        public Uri Image { get; set; }
+        public Images Images { get; set; }
+        public string Value { get; set; }
+        public string Suit { get; set; }
+    }
+
+    public partial class Images
+    {
+        public Uri Svg { get; set; }
+        public Uri Png { get; set; }
+    }
+}
+
